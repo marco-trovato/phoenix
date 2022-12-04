368e3bf51b493b53242eca50c9b9d5d66b8da791
Marco Trovato <link.inverse@gmail.com>
2022-12-04 15:12:33 +0100

    Delete terraform-script-aws-us directory

--- terraform-script-aws-us/README.md
+++
@@ -1,9 +0,0 @@
-# INSTRUCTIONS
-
-terraform init
-
-terraform plan -var-file="var.tfvars"
-
-terraform apply -var-file="var.tfvars"
-
-terraform destroy -var-file="var.tfvars"
--- terraform-script-aws-us/policies/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update -y && apt upgrade -y
-apt install git nodejs npm -y
-git clone https://github.com/claranet-ch/cloud-phoenix-kata.git
-cd cloud-phoenix-kata/
-npm update -g
-npm install
-npm start
